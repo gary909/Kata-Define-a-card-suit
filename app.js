aa7d449dfa6add72f98cd9584df3ea86563b4e40
gary <gary.white@lodgit.com>
2022-05-11 13:34:11 +0200

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function defineSuit(card) {
+    // good luck
+}
+
+console.log(defineSuit('Q♠')); // 'spades'
+console.log(defineSuit('9♦')); // 'diamonds'
+console.log(defineSuit('J♥')); // 'hearts'
